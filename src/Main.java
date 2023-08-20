14668ad906b673deb2eb8947bb6ad92689798204
etLanaBanana <svetlanaskovoroda@gmail.ru>
2023-08-20 21:54:26 +0300

    the sum of the numbers from 100 to 500

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,11 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+// Найти сумму всех целых чисел от 100 до 500;
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
-
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+        int sum = 0;
+        for (int i = 100; i < 501; i++) {
+            sum += i;
         }
+        System.out.println(sum);
     }
 }
